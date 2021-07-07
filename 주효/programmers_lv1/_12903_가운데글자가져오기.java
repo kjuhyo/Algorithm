26b69f56c2443f6eb4283b25c5962ffba9bda144
kjuhyo <dkrlwngy@gmail.com>
2021-07-07 17:57:19 +0900

    21-07-07

---
+++ 주효/programmers_lv1/_12903_가운데글자가져오기.java
@@ -0,0 +1,11 @@
+package 알고리즘깃;
+
+public class _12903_가운데글자가져오기 {
+	public String solution(String s) {
+	      String answer = "";
+	      int size=s.length();
+	      if(size%2==0) answer=s.substring(size/2-1, size/2+1);
+	      else answer=s.substring(size/2, size/2+1);
+	      return answer;
+	 }
+}
