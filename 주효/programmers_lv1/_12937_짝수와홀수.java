cba7487ace7d4369e9e7a19a9d557918d5ba665d
kjuhyo <dkrlwngy@gmail.com>
2021-07-13 23:49:16 +0900

    21-07-13

---
+++ 주효/programmers_lv1/_12937_짝수와홀수.java
@@ -0,0 +1,9 @@
+package 알고리즘깃;
+
+public class _12937_짝수와홀수 {
+	public String solution(int num) {
+	      String answer = "";
+	      answer= (num%2==0)? "Even":"Odd";
+	      return answer;
+	  }
+}
