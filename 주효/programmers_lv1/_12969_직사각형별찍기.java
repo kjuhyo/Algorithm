e990d6f265b877b69bd57dc007558fc95c8c2dc8
kjuhyo <dkrlwngy@gmail.com>
2021-07-19 23:31:04 +0900

    21-07-19

---
+++ 주효/programmers_lv1/_12969_직사각형별찍기.java
@@ -0,0 +1,23 @@
+package 알고리즘깃;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _12969_직사각형별찍기 {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        StringBuilder sb=new StringBuilder();
+        int a=Integer.parseInt(st.nextToken());
+        int b=Integer.parseInt(st.nextToken());
+        
+        for(int i=0; i<b; i++){
+            for(int j=0; j<a; j++){
+                sb.append("*");
+            }sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
