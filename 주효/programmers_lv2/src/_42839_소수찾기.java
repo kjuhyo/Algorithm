808c79b2cb6e34f82a7d761a6156794451cee296
kjuhyo <dkrlwngy@gmail.com>
2021-08-12 18:01:40 +0900

    21-08-12

--- 주효/programmers_lv2/src/_42839_소수찾기.java
+++ 주효/programmers_lv2/src/_42839_소수찾기.java
@@ -1,10 +1,93 @@
-import java.util.HashSet;
+import java.util.ArrayList;
 
 public class _42839_소수찾기 {
-	static char c[];
+
     static int N, answer;
     static boolean check[];
-    static HashSet<Integer> list;
+    static char isSelected[];
+    static char c[];
+    static ArrayList<Integer> list;
+    
+	public static int solution(String numbers) {
+		N=numbers.length();
+		check=new boolean[N];
+		isSelected=new char[N];
+		c=new char[N];
+		list=new ArrayList<>();
+	    for(int i=0; i<numbers.length(); i++){
+	        c[i]=numbers.charAt(i);
+	    }
+	    
+		for(int i=0; i<N; i++) {
+			for(int j=1; j<=N; j++) {
+				perm(i, j);	
+			}
+			
+		}
+		
+		return list.size();
+	}
+    
+
+    private static void perm(int idx, int range) { //순열인가
+		if(idx==range) {
+			String s="";
+            for(int i=0; i<range; i++){
+                s+=isSelected[i];
+            }
+           // System.out.println(s);
+            find(s);
+            return;
+		}
+		
+		for(int i=0; i<range; i++) {
+			if(!check[i]) {
+				check[i]=true;
+				isSelected[i]=c[idx];
+				perm(idx+1, range);
+				check[i]=false;
+			}
+		}
+	}
+
+
+	private static void find(String s) { //소수인지
+		if(!s.equals("")) {
+			s=s.replace(" ", "");
+        	int n=Integer.parseInt(s);
+        	if(n<2){
+        	    return;		
+        	} else if(n==2){
+                if(list.indexOf(n)<0){
+                        list.add(n);    
+                }
+                return;
+            } else {
+                for(int i=2; i<=Math.sqrt(n); i++){
+                    if(n%i==0){
+                        return;
+                    }
+                }
+                if(list.indexOf(n)<0){
+                        list.add(n);    
+                }
+                return;
+            }
+        	
+        }
+        return;
+	}
+
+
+	public static void main(String[] args) {
+    	System.out.println(solution("011"));
+    }
+    
+    //부분집합문제로 착각
+   /* static char c[];
+    static int N, answer;
+    static boolean check[];
+    static ArrayList<Integer> list;
     
     public int solution(String numbers) {
         N=numbers.length();
@@ -13,10 +96,9 @@ public class _42839_소수찾기 {
         for(int i=0; i<numbers.length(); i++){
             c[i]=numbers.charAt(i);
         }
-        list=new HashSet<>();
+        list=new ArrayList<>();
         powerSet(0);
-        
-       
+    
         return list.size();
     }
     public static void powerSet(int idx){
@@ -29,12 +111,25 @@ public class _42839_소수찾기 {
             }
             if(!s.equals("")) {
             	int n=Integer.parseInt(s);
-            	if(n!=1) {
-            		if(n%2==1 || n==2){
-            			//answer+=1;
-            			list.add(n);
-            		}
-            	}
+            	if(n<2){
+            	    return;		
+            	} else if(n==2){
+                    if(list.indexOf(n)<0){
+                            list.add(n);    
+                    }
+                    return;
+                } else {
+                    for(int i=2; i<=Math.sqrt(n); i++){
+                        if(n%i==0){
+                            return;
+                        }
+                    }
+                    if(list.indexOf(n)<0){
+                            list.add(n);    
+                    }
+                    return;
+                }
+            	
             }
             return;
         }
@@ -42,5 +137,9 @@ public class _42839_소수찾기 {
         powerSet(idx+1);
         check[idx]=false;
         powerSet(idx+1);
-    }
+    }*/
+    
+    
+    
+    
 }
