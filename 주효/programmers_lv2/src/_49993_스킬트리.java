5a684f01f474278599c30bd6f19433ecc003c95b
kjuhyo <dkrlwngy@gmail.com>
2021-09-02 18:17:49 +0900

    Update _49993_스킬트리.java

--- 주효/programmers_lv2/src/_49993_스킬트리.java
+++ 주효/programmers_lv2/src/_49993_스킬트리.java
@@ -1,39 +1,60 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class _49993_스킬트리 {
 
 	public static void main(String[] args) {
-		String arr[]= {"BACDE", "CBADF", "AECB", "BDA"};
-		String s="CBD";
-		System.out.println(solution(s,arr));
+		String arr[] = { "BACDE", "CBADF", "AECB", "BDA" };
+		String s = "CBD";
+		System.out.println(solution(s, arr));
 	}
-	
+
 	public static int solution(String skill, String[] skill_trees) {
-        int answer = 0;
-        for(int t=0; t<skill_trees.length; t++){
-            String st=skill_trees[t];
-            int idx=0; int currentLength=0;
-            String s="";
-           out: for(int i=0; i<skill.length(); i++){
-                for(int j=idx; j<st.length(); j++){
-                    if(skill.charAt(i)==st.charAt(j)){
-                        currentLength++;
-                        s+=st.charAt(j);
-                        if(skill.substring(0,currentLength).equals(s)){
-                        	idx=j;
-                            break;
-                        }else{
-                            break out;
-                        }
-                    }
-                }
-               if(skill.equals(s)){
-                  answer++;
-               }else {
-                   if(skill.contains(s)) answer++;
-               }
-            }
-        }
-        return answer;
-    }
+		int answer = 0;
+		for (int t = 0; t < skill_trees.length; t++) {
+			String st = skill_trees[t];
+			String s = "";
+
+			Queue<Character> q = new LinkedList<>();
+			for (int i = 0; i < skill.length(); i++) {
+				q.add(skill.charAt(i));
+			}
+
+			int idx = 0;
+			for (int j = idx; j < st.length(); j++) {
+				if (q.size() != 0) {
+					if (st.charAt(j) == q.peek()) {
+						q.poll();
+						idx = j;
+					}
+				}
+			}
+
+			if (q.size() == 0) {
+				answer += 1;
+			} else {
+				boolean flag = true;
+				out: while (!q.isEmpty()) {
+					for (int j = 0; j < st.length(); j++) {
+						if (q.peek() == st.charAt(j)) {
+							flag = false;
+							break out;
+						}
+					}
+					q.poll();
+				}
+
+				if (flag) {
+					answer += 1;
+				}
+
+			}
+
+			q.clear();
+		}
+
+		return answer;
+	}
 
 }
