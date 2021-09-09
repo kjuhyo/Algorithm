025702bf3cacbb60a983106a9991e179492d011c
kjuhyo <dkrlwngy@gmail.com>
2021-09-09 15:29:38 +0900

    Update _12913_땅따먹기.java

--- 주효/programmers_lv2/src/_12913_땅따먹기.java
+++ 주효/programmers_lv2/src/_12913_땅따먹기.java
@@ -1,52 +1,41 @@
+import java.util.Arrays;
 
 public class _12913_땅따먹기 {
-	static int dx[]={-1,1,0,0};
-    static int dy[]={0,0,-1,1};
-    static boolean check[][];
-    static int[][] map;
-    static int sum, max;
-    
-    public static int solution(int[][] land) {
-        int answer = 0;
-        map=land;
-        check=new boolean[map.length+1][4];
-        
-        sum=0; max=0;
-        for(int i=0; i<4; i++){
-            dfs(i, 0);   
-        }
-        return max;
-    }
-    
-    public static void dfs(int x, int y){
-        check[x][y]=true;
-        
-        for(int i=0; i<4; i++){
-            int nx=x+dx[i];
-            int ny=y+dy[i];
-            
-            if(nx<0 || ny<0 || nx>=map.length || ny>=4) return;
-            
-            else if(nx==map.length-1) {
-                check[nx][ny]=true;
-                sum+=map[nx][ny];
-                max=Math.max(sum, max);
-                return ;
-            }
-            
-            else if(!check[nx][ny] && x != nx && y!=ny){
-                check[nx][ny]=true;
-                sum+=map[nx][ny];
-                dfs(nx, ny);
-                check[nx][ny]=false;
-            }
-            
-        }
-        return;
-    }
-	    
-	    public static void main(String[] args) {
-	    	int[][] land= {{1,2,3,5},{5,6,7,8},{4,3,2,1}};
-			System.out.println(solution(land));
+
+	public static int solution(int[][] land) {
+		int answer = 0;
+		for(int i=1; i<land.length; i++) {
+			land[i][0]+=Math.max(Math.max(land[i-1][1], land[i-1][2]),land[i-1][3]);
+			land[i][1]+=Math.max(Math.max(land[i-1][0], land[i-1][2]),land[i-1][3]);
+			land[i][2]+=Math.max(Math.max(land[i-1][0], land[i-1][1]),land[i-1][3]);
+			land[i][3]+=Math.max(Math.max(land[i-1][0], land[i-1][1]),land[i-1][2]);
+		
+			//열이 4개(작은수)일 경우엔 이렇게도 가능하다.
+			if(i==land.length-1) {
+				answer=Math.max(Math.max(Math.max(land[i][0],land[i][1]),land[i][2]),land[i][3]);
+			}
 		}
+		return answer;
+	}
+	
+	public static int solution2(int[][] land) {
+		int answer = 0;
+		for(int i=1; i<land.length; i++) {
+			land[i][0]+=Math.max(Math.max(land[i-1][1], land[i-1][2]),land[i-1][3]);
+			land[i][1]+=Math.max(Math.max(land[i-1][0], land[i-1][2]),land[i-1][3]);
+			land[i][2]+=Math.max(Math.max(land[i-1][0], land[i-1][1]),land[i-1][3]);
+			land[i][3]+=Math.max(Math.max(land[i-1][0], land[i-1][1]),land[i-1][2]);	
+		}
+		
+		//만약 열 갯수가 많아서 일일이 Math.max할 수 없는 경우
+		int arr[]=land[land.length-1]; //land의 마지막 행
+		Arrays.sort(arr);
+		answer=arr[arr.length-1];
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		int[][] land = { { 1, 2, 3, 5 }, { 5, 6, 7, 8 }, { 4, 3, 2, 1 } };
+		System.out.println(solution2(land));
+	}
 }
